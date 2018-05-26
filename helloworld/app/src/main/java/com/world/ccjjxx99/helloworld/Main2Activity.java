8d6552f0d9a93f19da467b423055de6e0d977e98
ccjjxx99 <ccjjxx99@qq.com>
2018-05-26 13:34:01 +0800

    first Android

---
+++ helloworld/app/src/main/java/com/world/ccjjxx99/helloworld/Main2Activity.java
@@ -0,0 +1,13 @@
+package com.world.ccjjxx99.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout);
+    }
+}
